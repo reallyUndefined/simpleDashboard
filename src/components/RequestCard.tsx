aad14f908b6dfb7288d1d1cdd75271c29559f62a
Prabu <prabu.undefined@gmail.com>
2022-10-22 13:57:31 +0530

    RequestCard refactored

--- src/components/RequestCard.tsx
+++ src/components/RequestCard.tsx
@@ -3,22 +3,20 @@ import Card from "./Card";
 const RequestCard = () => (
   <Card>
     <h2 className="text-2xl font-semibold mb-2">Requests</h2>
-    <div className="flex justify-between items-end border-b pb-2 mb-2">
-      <span>Profile update request</span>
-      <span className="font-bold">21</span>
-    </div>
-    <div className="flex justify-between items-end border-b pb-2 mb-2">
-      <span>Leave request</span>
-      <span className="font-bold">13</span>
-    </div>
-    <div className="flex justify-between items-end border-b pb-2 mb-2">
-      <span>Loan request</span>
-      <span className="font-bold">06</span>
-    </div>
-    <div className="flex justify-between items-end">
-      <span>Other requests</span>
-      <span className="font-bold">18</span>
-    </div>
+    {[
+      { type: "Profile update request", value: 21 },
+      { type: "Leave request", value: 13 },
+      { type: "Loan request", value: 6 },
+      { type: "Other requests", value: 18 },
+    ].map((req) => (
+      <div
+        key={req.type}
+        className="flex justify-between items-end border-b pb-2 mb-2 last-of-type:pb-0 last-of-type:mb-0 last-of-type:border-none"
+      >
+        <span>{req.type}</span>
+        <span className="font-bold">{req.value}</span>
+      </div>
+    ))}
   </Card>
 );
 
