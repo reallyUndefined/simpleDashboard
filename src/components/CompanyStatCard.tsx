9d72648afbed6ec533d35027c588a902254d4c3b
Prabu <prabu.undefined@gmail.com>
2022-10-25 12:04:04 +0530

    dropdown menu

--- src/components/CompanyStatCard.tsx
+++ src/components/CompanyStatCard.tsx
@@ -1,5 +1,10 @@
 import Card from "./Card";
-import { AiOutlineLike, AiOutlineDislike } from "react-icons/ai";
+import {
+  AiOutlineLike,
+  AiOutlineDislike,
+  AiOutlineCaretDown,
+} from "react-icons/ai";
+import { useState } from "react";
 
 interface CompanyStatCardProps {
   title: string;
@@ -13,17 +18,45 @@ const CompanyStatCard = ({
   percent,
   isGood,
 }: CompanyStatCardProps) => {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [selectedOption, setSelectedOption] = useState("Q1 2021");
+
   return (
     <Card>
       <h2 className="text-2xl font-semibold">{title}</h2>
       <p className="mb-3">{description}</p>
       <div className="flex justify-between items-center">
-        <div className="relative">
-          <select className="w-full px-2 py-1 text-gray-500 bg-white border rounded-md shadow-sm outline-none focus:border-slate-300">
+        <div
+          className="relative flex gap-2 items-center border shadow-sm rounded-md px-2 py-1 cursor-pointer"
+          onClick={() => {
+            setShowDropdown((prev) => !prev);
+          }}
+        >
+          {/* <select className="w-full px-2 py-1 text-gray-500 bg-white border rounded-md shadow-sm outline-none focus:border-slate-300">
             {["Q1 2021", "Q2 2021", "Q3 2021", "Q4 2021"].map((option) => (
               <option className="bg-white hover:bg-slate-100">{option}</option>
             ))}
-          </select>
+          </select> */}
+          <span className="font-semibold">{selectedOption}</span>
+          <AiOutlineCaretDown />
+          <div
+            className={`absolute top-0 left-0 w-full border shadow-sm rounded-md overflow-hidden ${
+              showDropdown ? "visible" : "invisible"
+            }`}
+          >
+            <ul className="bg-white text-center">
+              {["Q1 2021", "Q2 2021", "Q3 2021", "Q4 2021"].map((option) => (
+                <li
+                  className="hover:bg-slate-100 px-2 py-1 cursor-pointer"
+                  onClick={() => {
+                    setSelectedOption(option);
+                  }}
+                >
+                  {option}
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
         <div className="flex gap-2">
           <span
